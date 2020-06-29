d0681892f0d7018e8054b177fb0a3ffef937367c
AnthonyLiu-MC <Aliu31@montgomerycollege.edu>
2020-06-29 17:55:54 -0400

    Add lab files

--- Lab6_JUnitTest/src/GradeBookTester.java
+++ Lab6_JUnitTest/src/GradeBookTester.java
@@ -34,26 +34,26 @@ class GradeBookTester {
 		assertTrue((book1.toString()).equals("78.0 56.0 "));
 		assertTrue((book2.toString()).equals("87.0 98.0 45.0 "));
 		
-		assertEquals(2, book1.getScoreSize());
-		assertEquals(3, book2.getScoreSize());
+		assertEquals(2, book1.getScoreSize(), 0.001);
+		assertEquals(3, book2.getScoreSize(), 0.001);
 	}
 
 	@Test
 	void testSum() {
-		assertEquals(134, book1.sum());
-		assertEquals(230, book2.sum());
+		assertEquals(134, book1.sum(), 0.001);
+		assertEquals(230, book2.sum(), 0.001);
 	}
 
 	@Test
 	void testMinimum() {
-		assertEquals(56.0, book1.minimum());
-		assertEquals(45.0, book2.minimum());
+		assertEquals(56.0, book1.minimum(),0.001);
+		assertEquals(45.0, book2.minimum(),0.001);
 	}
 
 	@Test
 	void testFinalScore() {
-		assertEquals(78, book1.finalScore());
-		assertEquals(185, book2.finalScore());
+		assertEquals(78, book1.finalScore(), 0.001);
+		assertEquals(185, book2.finalScore(), 0.001);
 	}
 
 }
